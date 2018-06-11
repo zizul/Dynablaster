5e6ac1fd20b13e03358d3463344b49e681839668
Kacper Zdanowicz <kacperz@man.poznan.pl>
2018-06-11 22:05:17 +0200

    destroy brick on explosion

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainController : MonoBehaviour {
+
+    private Vector3 lastPlayerPos;
+    public GameObject playerPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void SetPlayerLastPosition(Vector3 pos)
+    {
+        lastPlayerPos = pos;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Instantiate(
+            playerPrefab,
+            new Vector3(
+                Mathf.RoundToInt(lastPlayerPos.x),
+                Mathf.RoundToInt(lastPlayerPos.y),
+                Mathf.RoundToInt(lastPlayerPos.z)
+            ),
+            Quaternion.identity);
+        }
+    }
+}
